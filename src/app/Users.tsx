c7e701ee1cd987ad5ace213ac2c5b8d39a37f5db
israranjum <israranjum851@gmail.com>
2023-02-19 10:51:22 +0500

    first commit

---
+++ src/app/Users.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Users = () => {
+  return (
+    <div>
+      <h1 className="text-amber-600 font-bold">users texxt</h1>
+    </div>
+  );
+};
+
+export default Users;
