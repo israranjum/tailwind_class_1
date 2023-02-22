079242b28aded6e173c254ebac95b6febc7c5c41
israranjum <israranjum851@gmail.com>
2023-02-22 22:03:43 +0500

    header Section Completed

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,36 +1,58 @@
 import Image from "next/image";
 import React from "react";
+import SearchIcon from "./SearchIcon";
+import { AiFillHome } from "react-icons/ai";
+import { FiPlusCircle } from "react-icons/Fi";
 
 const Header = () => {
   return (
-    <div>
-      {/* left */}
-
-      <div className="flex items-center justify-between max-w-6xl pt-2">
-        <div className="cursor-pointer relative hidden lg:inline-grid">
-          <Image
-            src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
-            alt="Logo-img"
-            className="object-contain"
-            width={90}
-            height={70}
-          />
-        </div>
-        <div className="cursor-pointer  relative  lg:hidden">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
-            width={50}
-            height={50}
-            alt="Logo-img"
-            className="object-contain"
-          />
-        </div>
-        <h2>Right Side</h2>
+    <div className="flex items-center justify-between max-w-6xl pt-2 lg:mx-auto sm:mx-4">
+      <div className="cursor-pointer relative hidden lg:inline-grid">
+        <Image
+          src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
+          alt="Logo-img"
+          className="object-contain"
+          width={90}
+          height={70}
+        />
+      </div>
+      <div className="cursor-pointer  relative  lg:hidden">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
+          width={50}
+          height={50}
+          alt="Logo-img"
+          className="object-contain"
+        />
       </div>
 
       {/* Middle */}
+      <div className="relative">
+        <div className="absolute top-2 left-1 ">
+          <SearchIcon />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md "
+        />
+      </div>
 
       {/* Right */}
+
+      <div className="">
+        <div className="flex space-x-4 items-center">
+          <AiFillHome className="hidden md:inline-flex w-6 h-6 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out" />
+          <FiPlusCircle className="w-6 h-6 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out" />
+          <Image
+            src="/profile.jpg"
+            width={30}
+            height={35}
+            alt="Logo-img"
+            className="h-10 w-10 rounded-full object-cover cursor-pointer"
+          />
+        </div>
+      </div>
     </div>
   );
 };
